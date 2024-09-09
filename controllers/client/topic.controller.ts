80795af7c8e5613c3e7c06c89d78921ed0b6dec0
TaDinhTai <tadinhtai6868@gmail.com>
2024-09-09 09:54:12 +0700

    Trang danh sách bài hát (Client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
 
 // [GET] /topics/
---
+++ models/singer.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const singerSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    avatar: String,
+    status: String,
+    slug: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Singer = mongoose.model("Singer", singerSchema, "singers");
+
+export default Singer;
+
+// https://backend.daca.vn/assets/json/music-singers.json
